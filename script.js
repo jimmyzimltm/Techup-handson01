9c42b36df10b62adcdb60e0ea22a9856441d2cd4
jimmyzimltm <jimmy.zim@gmail.com>
2024-11-04 06:23:05 +0000

    Section C complete.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ age = age + 45
 
 var username = "preshant"
 var password = "techup"
-var isMale = true
+var isMale = false
 var isLoggedIn = false
 
 var blog_posts = ["Today was a good day", "The sky is blue", 
@@ -26,4 +26,31 @@ if (isMale == true) {
 
     console.log("You are male")
 
+} else {
+
+    console.log("You are not male")
+}
+
+while (age < 70) {
+
+    console.log("Your age is " + age );
+
+    age = age + 1;
+
+}
+
+
+function sayHello() {
+    alert("Hello everyone");
+}
+
+
+
+function showAge() {
+    // Get the value from the input field of ID #user-input
+    // Store this value in a variable called user_input
+    var user_input = document.getElementById("user-input").value
+    // Update the content for the HTML element with ID #header-age with
+    // whatever the user_input varible contains
+    document.getElementById("header-age").textContent = user_input
 }
